a66ace2a52b63a48fd3b2a00f2c2fe76c576fdb8
Soham Gupta <soham.gupta003@gmail.com>
2024-07-12 22:11:02 +0530

    Added Next Auth Integration

---
+++ components/ProfileImage.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { User } from "next-auth";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/DropdownMenu";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Image from "next/image";
+import { AvatarProps } from "@radix-ui/react-avatar";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+import { buttonVariants } from "@/components/ui/button";
+import { User as LUser }  from "lucide-react";
+
+interface ProfileImageProps {
+  user: Pick<User, "name" | "image" | "email">;
+}
+
+export default function ProfileImage({ user }: ProfileImageProps) {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <UserAvatar
+          className="h-8 w-8"
+          user={{
+            name: user.name || null,
+            image: user.image || null,
+          }}
+        />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="bg-white" align="end">
+        <div className="flex items-center justify-start gap-2 p-2">
+          <div className="flex flex-col space-y-1 leading-none">
+            {user.name && <p className="font-medium">{user.name}</p>}
+            {user.email && (
+              <p className="w-[200px] truncate text-sm text-zinc-700">
+                {user.email}
+              </p>
+            )}
+          </div>
+        </div>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href="/">Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/r/create">Create</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href="/settings">Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem
+          onSelect={(e) => {
+            e.preventDefault();
+            signOut({
+              callbackUrl: `${window.location.origin}/login`,
+            });
+          }}
+          className={`cursor-pointer ${buttonVariants({
+            variant: "destructive",
+          })} flex justify-center items-center`}
+        >
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+// User Avatar
+interface UserAvatarProps extends AvatarProps {
+  user: Pick<User, "name" | "image">;
+}
+function UserAvatar({ user, ...props }: UserAvatarProps) {
+  return (
+    <Avatar {...props}>
+      {user.image ? (
+        <div className="relative aspect-square h-full w-full">
+          <Image
+            fill
+            src={user.image}
+            alt="Profile Picture"
+            referrerPolicy="no-referrer"
+          />
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span className="sr-only">{user?.name}</span>
+          <LUser className="w-6 h-6" />
+        </AvatarFallback>
+      )}
+    </Avatar>
+  );
+}
